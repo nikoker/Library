309878848f542772ff85c26380a629dac0e0668b
Nikos Kermanidis <nikos.kermanidis@getbux.com>
2018-01-10 13:20:37 +0100

    library commit

--- library/src/androidTest/java/com/example/nikos/library/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/example/nikos/library/ExampleInstrumentedTest.java
@@ -22,5 +22,7 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.nikos.library", appContext.getPackageName());
+        // Test
+        //Another test
     }
 }
